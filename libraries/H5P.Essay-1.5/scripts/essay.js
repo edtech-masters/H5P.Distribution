4128baa96b69168a3d288a3f25f06cb4b8a67481
Muhammad Basit Tkxel <muhammad.basit@tkxel.com>
2022-09-27 19:57:26 +0500

    essay fix

--- libraries/H5P.Essay-1.5/scripts/essay.js
+++ libraries/H5P.Essay-1.5/scripts/essay.js
@@ -146,7 +146,7 @@ H5P.Essay = function ($, Question) {
     }
 
     // Create InputField
-    this.inputField = new H5P.Essay.InputField({
+    this.inputField =  Essay.prototype.InputField({
       'taskDescription': this.params.taskDescription,
       'placeholderText': this.params.placeholderText,
       'maximumLength': this.params.behaviour.maximumLength,
